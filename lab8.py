984e11a9eea20994185b8ba0ed9b785cf548517a
wilsonsong1 <G00418330@atu.ie>
2024-11-26 16:44:24 +0000

    added comments

--- lab8.py
+++ lab8.py
@@ -1,11 +1,15 @@
+#Import cv2 nump and matplotlib
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+#imported ATU jpeg using cv2
 img = cv2.imread('ATU.jpg',)
 
+#converted the image to grayscale
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+#displaying the grayscale image
 cv2.imshow('Grayscale', gray_image)
 
 cv2.waitKey(0)  
