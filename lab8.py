c56f9330d86c5181db1907007359e8619f6ebd6c
wilsonsong1 <G00418330@atu.ie>
2024-11-26 16:57:22 +0000

    Blurring image

--- lab8.py
+++ lab8.py
@@ -9,13 +9,15 @@ img = cv2.imread('ATU.jpg',)
 #converted the image to grayscale
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+imgOut = cv2.GaussianBlur(img,(13, 13),0)
+
 #Plotting the first coloured image
-plt.subplot(2, 1, 1),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), cmap = 'gray')
-plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(2, 2, 1),plt.imshow(cv2.cvtColor(imgOut, cv2.COLOR_BGR2RGB), cmap = 'gray')
+plt.title('1'), plt.xticks([]), plt.yticks([])
 
 #Plotting the gray scaled image
-plt.subplot(2, 1, 2),plt.imshow(gray_image, cmap = 'gray')
-plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
+plt.subplot(2, 2, 2),plt.imshow(gray_image, cmap = 'gray')
+plt.title('2'), plt.xticks([]), plt.yticks([])
 
 #displaying the image
 plt.show()
