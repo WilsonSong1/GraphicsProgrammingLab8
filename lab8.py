20b61fa95a61564b5ea0b1d6e4fff67a44c6de60
wilsonsong1 <G00418330@atu.ie>
2024-11-26 16:43:27 +0000

    display image

--- lab8.py
+++ lab8.py
@@ -5,3 +5,8 @@ from matplotlib import pyplot as plt
 img = cv2.imread('ATU.jpg',)
 
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow('Grayscale', gray_image)
+
+cv2.waitKey(0)  
+cv2.destroyAllWindows()
