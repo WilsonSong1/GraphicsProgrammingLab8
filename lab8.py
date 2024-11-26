522700801315f077fbe3f5816058617819c736b5
wilsonsong1 <G00418330@atu.ie>
2024-11-26 16:48:39 +0000

    Plotted 2 images

--- lab8.py
+++ lab8.py
@@ -9,8 +9,16 @@ img = cv2.imread('ATU.jpg',)
 #converted the image to grayscale
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-#displaying the grayscale image
-cv2.imshow('Grayscale', gray_image)
+#Plotting the first coloured image
+plt.subplot(2, 1, 1),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), cmap = 'gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+
+#Plotting the gray scaled image
+plt.subplot(2, 1, 2),plt.imshow(gray_image, cmap = 'gray')
+plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
+
+#displaying the image
+plt.show()
 
 cv2.waitKey(0)  
 cv2.destroyAllWindows()
